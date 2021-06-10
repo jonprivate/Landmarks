6e003a7374f6f3565a07c7d5225e588a41dab2cb
Jiong Liu <jonprivate1991@gmail.com>
2021-06-09 23:25:31 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hai Xu on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
